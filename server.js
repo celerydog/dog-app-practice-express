d683b371c8b26dad617f2d395c1f1eb90ccd639c
celerydog <lizzy.eden@gmail.com>
2025-04-15 15:33:37 +1200

    remove debugging

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const PORT = 6969
 require('dotenv').config()
 
 let db
-console.log("DB_String:", process.env.DB_String); // add this
 let dbConnectionStr = process.env.DB_String // to ask Jeff what doing
 let dbName = 'dogs'
 
